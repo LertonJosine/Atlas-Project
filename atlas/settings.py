e11a15750fc474dfa245201f39f38c52c9181958
LertonJosine <josinelerton@gmail.com>
2024-09-12 21:22:43 +0200

    landing page

--- atlas/settings.py
+++ atlas/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'atlas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath("templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
